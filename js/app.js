28de28b8ad03238a747dba6483777a3e9d367d4b
bushmusi <bushera.mestofa@gmail.com>
2022-02-02 00:21:10 +0300

    Update js file

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
-const event_tag_id = ['open-menu', 'onclick-close','onclick-portfolio', 'onclick-about', 'onclick-contact']
-event_tag_id.forEach(
-    addEvent
-)
-
-function addEvent(event_tag_id){
-    document.getElementById(event_tag_id).addEventListener('click', () => {
-            openCloseMenu();
-        })
-}
+const eventTagId = ['open-menu', 'onclick-close', 'onclick-portfolio', 'onclick-about', 'onclick-contact'];
 
 function openCloseMenu() {
-    const x = document.querySelector('.mobile-menu-list');
-    const y = document.querySelector('body');
-    if (x.style.display === 'flex') {
-      x.style.display = 'none';
-      y.style.overflow = 'scroll';
-    } else {
-      x.style.display = 'flex';
-      y.style.overflow = 'hidden';
-    }
+  const x = document.querySelector('.mobile-menu-list');
+  const y = document.querySelector('body');
+  if (x.style.display === 'flex') {
+    x.style.display = 'none';
+    y.style.overflow = 'scroll';
+  } else {
+    x.style.display = 'flex';
+    y.style.overflow = 'hidden';
   }
+}
+
+function addEvent(eventTagId) {
+  document.getElementById(eventTagId).addEventListener('click', () => {
+    openCloseMenu();
+  });
+}
+
+eventTagId.forEach(
+  addEvent,
+);
