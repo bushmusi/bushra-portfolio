5f0142639c931bae052f5028e32f21f32eec4e8e
bushmusi <bushera.mestofa@gmail.com>
2022-02-04 16:10:05 +0300

    Validation function and error msg add

--- js/form-validate.js
+++ js/form-validate.js
@@ -1,58 +1,20 @@
-function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector('small')
-	msg.innerText = message
-	// update the class for the input
-	input.className = type ? 'success' : 'error'
-	return type
+function validateEmail(emailContent){
+    console.log(emailContent);
+    if(emailContent !== emailContent.toLowerCase())
+        return false;
+    return true;
 }
 
-function showError(input, message) {
-	return showMessage(input, message, false)
-}
+const form = document.getElementById('submit-form');
+const EMAIL_INVALID = "Email address should be small letters only. you may use:"
 
-function showSuccess(input) {
-	return showMessage(input, '', true)
-}
-
-function hasValue(input, message) {
-	if (input.value.trim() === '') {
-		return showError(input, message)
-	}
-	return showSuccess(input)
-}
-
-function validateEmail(input, requiredMsg, invalidMsg) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false
-	}
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-	const email = input.value.trim()
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg)
-	}
-	return true
-}
-
-const form = document.querySelector('#signup')
-
-const NAME_REQUIRED = 'Please enter your name'
-const EMAIL_REQUIRED = 'Please enter your email'
-const EMAIL_INVALID = 'Please enter a correct email address format'
-
-form.addEventListener('submit', function (event) {
-	// stop form submission
-	event.preventDefault()
-
-	// validate the form
-	let nameValid = hasValue(form.elements['name'], NAME_REQUIRED)
-	let emailValid = validateEmail(form.elements['email'], EMAIL_REQUIRED, EMAIL_INVALID)
-	// if valid, submit the form.
-	if (nameValid && emailValid) {
-		alert('Demo only. No form was posted.')
-	}
+form.addEventListener('submit',(event) => {
+    event.preventDefault();
+    let emailText = form.elements['email'].value;
+    let isEmailValid = validateEmail(emailText)
+    if(isEmailValid){
+        form.submit();
+    }
+    const msgBox = document.querySelector('.msg-box');
+    msgBox.innerHTML = `<small>${EMAIL_INVALID} <i>${emailText.toLowerCase()}<i></small>`;
 })
