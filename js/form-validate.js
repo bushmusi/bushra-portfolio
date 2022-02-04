28e8a5652b6ef5a89ebaad56e1f546c1ffce252f
bushmusi <bushera.mestofa@gmail.com>
2022-02-04 16:22:46 +0300

    Fix eslint errors

--- js/form-validate.js
+++ js/form-validate.js
@@ -1,20 +1,21 @@
-function validateEmail(emailContent){
-    console.log(emailContent);
-    if(emailContent !== emailContent.toLowerCase())
-        return false;
-    return true;
+function validateEmail(emailContent) {
+  const lowerEmail = emailContent.toLowerCase();
+  if (emailContent !== lowerEmail) {
+    return false;
+  }
+  return true;
 }
 
 const form = document.getElementById('submit-form');
-const EMAIL_INVALID = "Email address should be small letters only. you may use:"
+const EMAIL_INVALID = 'Email address should be small letters only. you may use:';
 
-form.addEventListener('submit',(event) => {
-    event.preventDefault();
-    let emailText = form.elements['email'].value;
-    let isEmailValid = validateEmail(emailText)
-    if(isEmailValid){
-        form.submit();
-    }
-    const msgBox = document.querySelector('.msg-box');
-    msgBox.innerHTML = `<small>${EMAIL_INVALID} <i>${emailText.toLowerCase()}<i></small>`;
-})
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailText = form.elements.email.value;
+  const isEmailValid = validateEmail(emailText);
+  if (isEmailValid) {
+    form.submit();
+  }
+  const msgBox = document.querySelector('.msg-box');
+  msgBox.innerHTML = `<small>${EMAIL_INVALID} <i>${emailText.toLowerCase()}<i></small>`;
+});
